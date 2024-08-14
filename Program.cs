43ace4c7a36e343eecadf116c100a2c2f061b596
Lucas Antonio <prolucasa2000@gmail.com>
2024-08-13 21:19:47 -0300

    só basico

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Ola, mundo!");
+
+
+
+Console.WriteLine("Digite um numero para A ");
+var a = Console.ReadLine();
+Console.WriteLine("Digite um Valor para B");
+var b = Console.ReadLine();
+
+var soma = (Convert.ToDouble(a) + Convert.ToDouble(b)) / 2;
+
+
+Console.WriteLine("A media dos valores é " + soma);
+
+if (soma > 60){
+    Console.WriteLine("Aprovado");
+}
+else{
+    Console.WriteLine("Reprovado");
+}
